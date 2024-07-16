9a8a260772c1fb204d93cc4bf34109a2485b7c74
Ayush-2928 <ayushmangla03@gmail.com>
2024-07-16 17:22:46 +0530

    2nd

--- DAY2/star5.java
+++ DAY2/star5.java
@@ -3,12 +3,15 @@ package DAY2;
 public class star5 {
     public static void main(String[] args) {
         int n=5;
-        for (int i = 1; i <=n ; i++) {
-            for (int k = 1; k <=(n-i); k++) {
+        for (int i = n; i >=1 ; i--) {
+            //for space
+            for (int k = 1; k <=n-i; k++) {
                 
                 System.out.print(" ");
+           
             }
-            for (int j = 5; j >=i; j--) {
+            //for star
+            for (int j = 1; j <=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
--- DAY2/starques1.java
+++ DAY2/starques1.java
@@ -4,6 +4,7 @@ public class starques1 {
     public static void main(String[] args) {
         int n=5;
         // n controls no of rows , j controls what will be printed inside 
+        
         for (int i = 1; i <=n  ; i++) {
             for (int j = 1; j <=  n; j++) {
                 System.out.print( "*" + " ");
