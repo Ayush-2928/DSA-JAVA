f9a2b8ee5322989f8af9ea4948a0279f9438560b
Ayush-2928 <ayushmangla03@gmail.com>
2024-09-08 10:34:12 +0530

    recursion final

--- Week8/recursion2.java
+++ Week8/recursion2.java
@@ -6,9 +6,11 @@ public class recursion2 {
     public static void main(String[] args) {
        // System.out.println(fibonacciSeries(5));
        // generate_subsequences("Car", "");
-       System.out.println(countSubSequence("Car"));
+   //    System.out.println(countSubSequence("Car"));
        // head_tail(2, "");
       //  System.out.println(countHeadTail(3));
+    //  parenthesis(3, "", 0, 0);
+      permutations("abc", "");
 
        
     }
@@ -54,10 +56,31 @@ public class recursion2 {
         return x;
     }
 //generate valid parenthesis 
-    static void parenthesis (int n,String ans){
-        
+    static void parenthesis (int n,String ans, int l, int r){
+        if (l==n && r==n) {
+            System.out.println(ans);
+            return;
+        }
+        if (l<n) {
+            parenthesis(n, ans + "{", l+1, r);
+        }
+        if (r<l) {
+            parenthesis(n, ans + "}", l, r+1);
+        }
     }
+//permutations for a string - eg "abc"
+    static void permutations(String ques,String ans){
+        if (ques.length()==0) {
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i < ques.length(); i++) {
+            String s1=ques.substring(0, i);//before b (for middle case)
+            String s2=ques.substring(i+1);//
+            permutations(s1+s2, ans+ ques.charAt(i));
+        }
 
+    }
     }
 
 
